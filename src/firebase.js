59025722deea8bc56c96934a9aef507880816863
uzmazafeer <uzmazafeer483@gmail.com>
2026-02-26 21:00:05 +0500

    third cmmit

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,8 @@ const firebaseConfig = {
   storageBucket: "saylanihack2026.firebasestorage.app",
   messagingSenderId: "835399617062",
   appId: "1:835399617062:web:fbfcf8ad91859ee0beb4e7",
-  measurementId: "G-XB4C6BVGJ9"
+  measurementId: "G-XB4C6BVGJ9",
+  databaseUrl: "https://saylanihack2026-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig)
